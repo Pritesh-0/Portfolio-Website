26b0c409ab61e79c25704cc3899138d18c83c618
Pritesh Agrawal <priteshagrawal0@gmail.com>
2022-09-20 20:42:15 +0530

    Fixed class to classname added cursor interaction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,21 +8,21 @@ const Navbar = () => {
         <div class="max-w-7xl mx-auto px-8">
             <div class="flex items-center justify-between h-16">
                 <div class="w-full justify-between flex lg:items-center sm:flex-row">
-                    <div class="flex-shrink-0 h-8 w-8 text-[#001219] text-4xl text-mono" ><Link to="hero" spy={true} smooth={true} offset={0} duration={500}>
+                    <div class="cursor-pointer flex-shrink-0 h-8 w-8 text-[#001219] text-4xl text-mono" ><Link to="hero" spy={true} smooth={true} offset={0} duration={500}>
                         P_A0
                     </Link></div>
                     <div class="hidden md:block">
-                        <div class="ml-10 flex items-baseline space-x-4 text-slate-800 text-semibold">
-                            <Link to="About" spy={true} smooth={true} offset={0} duration={500} class=" focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
+                        <div class="cursor-pointer ml-10 flex items-baseline space-x-4 text-slate-800 text-semibold">
+                            <Link to="About" spy={true} smooth={true} offset={0} duration={500}  className=" focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 About
                             </Link>
-                            <Link to="Skills" spy={true} smooth={true} offset={0} duration={500} class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
+                            <Link to="Skills" spy={true} smooth={true} offset={0} duration={500} className="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 Skills
                             </Link>
-                            <Link to="Achievements" spy={true} smooth={true} offset={0} duration={500} class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
+                            <Link to="Achievements" spy={true} smooth={true} offset={0} duration={500} className="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 Achievements
                             </Link>
-                            <Link to="Projects" spy={true} smooth={true} offset={0} duration={500} class=" focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
+                            <Link to="Projects" spy={true} smooth={true} offset={0} duration={500} className=" focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 Projects
                             </Link>
                         </div>
@@ -38,17 +38,17 @@ const Navbar = () => {
             </div>
         </div>
         <div class="md:hidden">
-            <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3 text-slate-800">
-                <Link to="About" spy={true} smooth={true} offset={0} duration={500} class="focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
+            <div class="cursor-pointer px-2 pt-2 pb-3 space-y-1 sm:px-3 text-slate-800">
+                <Link to="About" spy={true} smooth={true} offset={0} duration={500} className="focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                     About
                 </Link>
-                <Link to="Skills" spy={true} smooth={true} offset={0} duration={500}  class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Skills">
+                <Link to="Skills" spy={true} smooth={true} offset={0} duration={500}  className="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Skills">
                     Skills
                 </Link>
-                <Link to="Achievements" spy={true} smooth={true} offset={-50} duration={500}  class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Achievements">
+                <Link to="Achievements" spy={true} smooth={true} offset={-50} duration={500}  className="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Achievements">
                     Achievements
                 </Link>
-                <Link to="Projects" spy={true} smooth={true} offset={0} duration={500}  class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Projects">
+                <Link to="Projects" spy={true} smooth={true} offset={0} duration={500}  className="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Projects">
                     Projects
                 </Link>
             </div>
