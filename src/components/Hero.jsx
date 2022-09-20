5ee2317057567dca51fad8a5174b481aacb5109e
Pritesh Agrawal <priteshagrawal0@gmail.com>
2022-09-20 19:52:16 +0530

    added smooth scrolling functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Typed from 'react-typed';
 
+
 const Hero = () => {
   return (
     
-<div class="bg-[#001219] dark:bg-gray-800 pt-60">
+<div class="bg-[#001219] dark:bg-gray-800 pt-60" id ='hero'>
     <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
         <h2 class="text-3xl font-extrabold text-[#005f73] dark:text-white sm:text-4xl">
             <span class="block">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
   return (
@@ -7,23 +8,23 @@ const Navbar = () => {
         <div class="max-w-7xl mx-auto px-8">
             <div class="flex items-center justify-between h-16">
                 <div class="w-full justify-between flex lg:items-center sm:flex-row">
-                    <a class="flex-shrink-0 h-8 w-8 text-[#001219] text-4xl text-mono" href="/">
+                    <div class="flex-shrink-0 h-8 w-8 text-[#001219] text-4xl text-mono" ><Link to="hero" spy={true} smooth={true} offset={0} duration={500}>
                         P_A0
-                    </a>
+                    </Link></div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4 text-slate-800 text-semibold">
-                            <a class=" focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#About">
+                            <Link to="About" spy={true} smooth={true} offset={0} duration={500} class=" focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 About
-                            </a>
-                            <a class="  focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#Skills">
+                            </Link>
+                            <Link to="Skills" spy={true} smooth={true} offset={0} duration={500} class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 Skills
-                            </a>
-                            <a class="  focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#Achievements">
+                            </Link>
+                            <Link to="Achievements" spy={true} smooth={true} offset={0} duration={500} class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 Achievements
-                            </a>
-                            <a class=" focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#Projects">
+                            </Link>
+                            <Link to="Projects" spy={true} smooth={true} offset={0} duration={500} class=" focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                                 Projects
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
@@ -38,18 +39,18 @@ const Navbar = () => {
         </div>
         <div class="md:hidden">
             <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3 text-slate-800">
-                <a  class="  focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#About">
+                <Link to="About" spy={true} smooth={true} offset={0} duration={500} class="focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium">
                     About
-                </a>
-                <a  class="  focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#Skills">
+                </Link>
+                <Link to="Skills" spy={true} smooth={true} offset={0} duration={500}  class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Skills">
                     Skills
-                </a>
-                <a  class="  focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#Achievements">
+                </Link>
+                <Link to="Achievements" spy={true} smooth={true} offset={-50} duration={500}  class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Achievements">
                     Achievements
-                </a>
-                <a  class="  focus:text-[#94d2bd] dark:text-white  hover:text-[#94d2bd] dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#Projects">
+                </Link>
+                <Link to="Projects" spy={true} smooth={true} offset={0} duration={500}  class="  focus:text-[#94d2bd] hover:text-[#94d2bd] px-3 py-2 rounded-md text-sm font-medium" href="/#Projects">
                     Projects
-                </a>
+                </Link>
             </div>
         </div>
     </nav>
